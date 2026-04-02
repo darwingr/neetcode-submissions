7ed181053d93257529afbd37566d3346a45b8998
Darwin Groskleg <darwingr@users.noreply.github.com>
2026-04-02 15:15:15 -0400

    Add: house-robber - submission-8

---
+++ Data Structures & Algorithms/house-robber/submission-8.cpp
@@ -0,0 +1,20 @@
+// O(N)
+// O(1)
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int last = -1;
+        int before_last = -1;
+
+        int best;
+        for (int i=nums.size()-1; i>=0; i--) {
+            best = max(
+                nums[i] + max(0, before_last),
+                max(0, last)
+            );
+            before_last = last;
+            last = best;
+        }
+        return best;
+    }
+};
