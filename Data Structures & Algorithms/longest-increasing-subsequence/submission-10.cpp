dac04830764ea82bcd767d995f97ea193a1d9ce6
Darwin Groskleg <darwingr@users.noreply.github.com>
2026-04-03 16:02:47 -0400

    Add: longest-increasing-subsequence - submission-10

---
+++ Data Structures & Algorithms/longest-increasing-subsequence/submission-10.cpp
@@ -0,0 +1,29 @@
+// tails + BINARY SEARCH: Patience Sorting
+// O(N log N)
+// O(N)
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        // lengths2tails
+        //      index k  → subsequence length = k + 1
+        //      value    → smallest tail achievable for that length
+        vector<int> tails;
+        tails.push_back(nums[0]);
+
+        for (int num : nums) {
+            // Can num extend the longest subseq so far?
+            if (tails.back() < num)
+                tails.push_back(num);
+            
+            // Find the longest subsequence that num can extend, and update its tail.
+            else {
+                //  O(log N) - find index of first val >= nums[i]
+                //  index = number of elements in tails that are strictly less than num.
+                int index = ranges::lower_bound(tails, num) - tails.begin();
+                //  Best tail for subsequences of size index is now num.
+                tails[index] = num;
+            }
+        }
+        return tails.size();
+    }
+};
