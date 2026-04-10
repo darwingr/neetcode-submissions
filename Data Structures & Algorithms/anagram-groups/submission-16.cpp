9392b737bb8b313e57905f0604de45aab5c82818
Darwin Groskleg <darwingr@users.noreply.github.com>
2026-04-10 15:59:12 -0400

    Add: anagram-groups - submission-16

---
+++ Data Structures & Algorithms/anagram-groups/submission-16.cpp
@@ -0,0 +1,30 @@
+// Encode freqencies to delimited string
+//  O(m x n)
+//  O(m)
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> anagroups;
+        for (string& word : strs) {
+            auto code = alphaCount(word);
+            anagroups[code].push_back(word);
+        }
+        vector<vector<string>> result;
+        for (auto& [_, anag] : anagroups)
+            result.push_back(anag);
+        return result;
+    }
+
+
+    string alphaCount(string& word) {
+        vector<int> counts(26);
+        for (char c : word)
+            counts[c - 'a']++;
+        string code;
+        for (int n : counts) {
+            code += to_string(n);
+            code += ',';
+        }
+        return code;
+    }
+};
