8ef86a97ca4891eff806f7603e4c97423ea1f7ac
Darwin Groskleg <darwingr@users.noreply.github.com>
2026-04-10 15:31:59 -0400

    Add: reverse-bits - submission-4

---
+++ Data Structures & Algorithms/reverse-bits/submission-4.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    uint32_t reverseBits(uint32_t n) {
+        uint32_t res = 0;
+        int bit = 32;
+        while (bit--) {
+            int lsb = n % 2 == 0 ? 0 : 1;
+            res += lsb * pow(2, bit);
+            n = n >> 1;
+        }
+        return res;
+    }
+};
