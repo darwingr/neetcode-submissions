e737864549f97c08c18c4ad6588f3a1ed6fb87ce
Darwin Groskleg <darwingr@users.noreply.github.com>
2026-04-10 14:47:10 -0400

    Add: count-paths - submission-1

---
+++ Data Structures & Algorithms/count-paths/submission-1.cpp
@@ -0,0 +1,20 @@
+// DP - bottom up, carry forward
+//  O(m * n)
+//  O(m * n)
+class Solution {
+    vector<vector<int>> paths_from;
+public:
+    int uniquePaths(int m, int n) {
+        paths_from = vector(m, vector<int>(n));
+        paths_from.back().back() = 1;
+        for (int i=m-1; i>=0; i--) {
+            for (int j=n-1; j>=0; j--) {
+                if (j+1 < n)
+                    paths_from[i][j] += paths_from[i][j+1];
+                if (i+1 < m)
+                    paths_from[i][j] += paths_from[i+1][j];
+            }
+        }
+        return paths_from.front().front();
+    }
+};
