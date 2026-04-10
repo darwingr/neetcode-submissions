f9c40f66d387905c4e5cc56c4c17cf83ab0021f2
Darwin Groskleg <darwingr@users.noreply.github.com>
2026-04-10 14:20:57 -0400

    Add: clone-graph - submission-4

---
+++ Data Structures & Algorithms/clone-graph/submission-4.cpp
@@ -0,0 +1,44 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
+// Map old to new
+//  O(V + E)
+//  O(V)
+class Solution {
+    unordered_map<Node*, Node*> old_to_new;
+public:
+    Node* cloneGraph(Node* node) {
+        if (!node) return nullptr;
+        old_to_new.clear();
+        return cloneSubGraph(node);
+    }
+private:
+    Node* cloneSubGraph(Node* node) {
+        Node* clone = new Node(node->val);
+        old_to_new[node] = clone;
+        for (auto nei : node->neighbors) {
+            if (!old_to_new.contains(nei))
+                cloneSubGraph(nei);
+            clone->neighbors.push_back(old_to_new[nei]);
+        }
+        return clone;
+    }
+};
