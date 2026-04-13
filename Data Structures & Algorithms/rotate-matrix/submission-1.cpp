83893860c48123676bac6fe54f6c35b9c64788ff
Darwin Groskleg <darwingr@users.noreply.github.com>
2026-04-13 15:19:54 -0400

    Add: rotate-matrix - submission-1

---
+++ Data Structures & Algorithms/rotate-matrix/submission-1.cpp
@@ -0,0 +1,13 @@
+// Reverse Rows & transpose
+//  O(N^2)
+//  O(1)
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        ranges::reverse(matrix);
+
+        for (int i=0; i<matrix.size(); i++)
+            for (int j=1+i; j<matrix.size(); j++)
+                swap(matrix[i][j], matrix[j][i]);
+    }
+};
