84dc798086f08530aad8fb12cc989983455d484a
Darwin Groskleg <darwingr@users.noreply.github.com>
2026-04-13 17:48:50 -0400

    Add: course-schedule - submission-14

---
+++ Data Structures & Algorithms/course-schedule/submission-14.cpp
@@ -0,0 +1,33 @@
+// Find cycles
+//  O(V + E)
+//  O(V + E)
+class Solution {
+    vector<vector<int>> prereqs;
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        prereqs = vector(numCourses, vector<int>());
+        for (auto cp : prerequisites)
+            prereqs[cp[0]].push_back(cp[1]);
+
+        for (int i=0; i<numCourses; i++) {
+            unordered_set<int> visited;
+            if (containsCycle(i, visited))
+                return false;
+        }
+        return true;
+    }
+
+    // dfs
+    bool containsCycle(int course, unordered_set<int>& visited) {
+        if (visited.contains(course))
+            return true;
+        visited.insert(course);
+
+        for (int p : prereqs[course])
+            if (containsCycle(p, visited))
+                return true;
+
+        visited.erase(course);
+        return false;
+    }
+};
