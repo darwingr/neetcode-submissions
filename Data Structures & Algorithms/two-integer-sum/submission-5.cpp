3e470da55cc1c92bde4253cc4b82a031312b0ac0
Darwin Groskleg <darwingr@users.noreply.github.com>
2026-04-18 18:24:44 -0400

    Add: two-integer-sum - submission-5

---
+++ Data Structures & Algorithms/two-integer-sum/submission-5.cpp
@@ -0,0 +1,15 @@
+// Hash diffs, math
+//  O(N)
+//  O(N)
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> diff_idx(nums.size());
+        for (int i=0; i<nums.size(); i++) {
+            if (diff_idx.contains(nums[i]))
+                return { diff_idx[nums[i]], i };
+            int diff = target - nums[i];
+            diff_idx[diff] = i;
+        }
+    }
+};
