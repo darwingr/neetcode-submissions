b61da3463fa9b8d9d6c32809d11900a0f0179c01
Darwin Groskleg <darwingr@users.noreply.github.com>
2026-04-20 18:42:28 -0400

    Add: three-integer-sum - submission-7

---
+++ Data Structures & Algorithms/three-integer-sum/submission-7.cpp
@@ -0,0 +1,39 @@
+// 2-pointer & sort
+//  O(N^2)
+//  O(1)
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        ranges::sort(nums);
+
+        vector<vector<int>> out;
+        for (int i=0; i<nums.size()-2; i++) {
+            if (i>0 && nums[i-1] == nums[i])
+                continue;
+            int l = i+1;
+            int r = nums.size() - 1;
+
+            while (l<r) {
+                if (l-1 > i && nums[l] == nums[l-1]) {
+                    l++;
+                    continue;
+                }
+                if (r+1 < nums.size() && nums[r] == nums[r+1]) {
+                    r--;
+                    continue;
+                }
+                int sum = nums[i] + nums[l] + nums[r];
+                if (sum == 0) {
+                    out.push_back({nums[i], nums[l], nums[r]});
+                    l++;
+                    r--;
+                }
+                else if (sum < 0)
+                    l++;
+                else
+                    r--;
+            }
+        }
+        return out;
+    }
+};
