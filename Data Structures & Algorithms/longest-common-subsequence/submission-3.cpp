ccc1ccb79d937907886777b3c3c11b86d0c60914
Darwin Groskleg <darwingr@users.noreply.github.com>
2026-04-20 20:19:17 -0400

    Add: longest-common-subsequence - submission-3

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-3.cpp
@@ -0,0 +1,19 @@
+// Every combo DP
+//  O(M * N)
+//  O(M * N)
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        vector<vector<int>> lengths(text1.size()+1, vector<int>(text2.size()+1));
+        for (int i=text1.size()-1; i>=0; i--)
+            for (int j=text2.size()-1; j>=0; j--) {
+                if (text1[i] == text2[j])
+                    lengths[i][j] = 1 + lengths[i+1][j+1];
+                else
+                    lengths[i][j] = max(lengths[i+1][j],
+                                        lengths[i][j+1]);
+            }
+
+        return lengths[0][0];
+    }
+};
