e7719e3647c6f932c4a516719c1df86356acacb9
Darwin Groskleg <darwingr@users.noreply.github.com>
2026-04-22 04:28:25 -0400

    Add: find-target-in-rotated-sorted-array - submission-5

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-5.cpp
@@ -0,0 +1,29 @@
+// Bin Search: one pass
+//  O(log N)
+//  O(1)
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l=0;
+        int r = nums.size() - 1;
+
+        while (l<=r) {
+            int mid = (l + r) / 2;
+            if (target == nums[mid])
+                return mid;
+
+            if (nums[l] <= nums[mid]) {
+                if (target < nums[l] || target > nums[mid])
+                    l = mid + 1;
+                else
+                    r = mid - 1;
+            } else {
+                if (target < nums[mid] || target > nums[r])
+                    r = mid - 1;
+                else
+                    l = mid + 1;
+            }
+        }
+        return -1;
+    }
+};
