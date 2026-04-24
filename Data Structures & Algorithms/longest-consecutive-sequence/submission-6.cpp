9cada7102e113233c454d730b96d6382ed372e78
Darwin Groskleg <darwingr@users.noreply.github.com>
2026-04-24 16:57:47 -0400

    Add: longest-consecutive-sequence - submission-6

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-6.cpp
@@ -0,0 +1,33 @@
+// Track Starts, Count Diff
+//  O(N)
+//  O(N)
+class Solution {
+public:
+    int longestConsecutive(vector<int>& nums) {
+        unordered_map<int, int> start_indices;
+
+        // 3 options: extend, begin, join
+        for (int i=0; i<nums.size(); i++) {
+            if (start_indices.contains(nums[i]))
+                continue;
+            start_indices[nums[i]] = i;
+            int prev = nums[i] - 1;
+            int next = nums[i] + 1;
+            if (start_indices.contains(prev))
+                start_indices[nums[i]] = start_indices[prev];
+            if (start_indices.contains(next))
+                start_indices[next] = start_indices[nums[i]];
+        }
+
+        int biggest = 0;
+        for (auto [val, start_i] : start_indices) {
+            if (val == nums[start_i]) {
+                int end_val = val;
+                while (start_indices.contains(++end_val)) {}
+                int diff = end_val - val;
+                biggest = max(diff, biggest);
+            }
+        }
+        return biggest;
+    }
+};
