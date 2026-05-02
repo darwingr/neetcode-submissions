d749e2ec0590bf543ad96519682e561471cd4153
Darwin Groskleg <darwingr@users.noreply.github.com>
2026-05-02 17:33:53 -0400

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.cpp
@@ -0,0 +1,18 @@
+// 2-pointers L & R, inc L if
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int small = 0;
+        int big = numbers.size()-1;
+        int sum = numbers[small] + numbers[big];
+        
+        while (sum != target) {
+            if (sum < target)
+                small++;
+            else
+                big--;
+            sum = numbers[small] + numbers[big];
+        }
+        return {small+1, big+1};
+    }
+};
