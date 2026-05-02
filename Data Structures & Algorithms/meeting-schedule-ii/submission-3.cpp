2e3423b825d390b6899e4ee475a2e61017c2dea3
Darwin Groskleg <darwingr@users.noreply.github.com>
2026-05-01 21:34:31 -0400

    Add: meeting-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-3.cpp
@@ -0,0 +1,30 @@
+// Line Sweep: ordered map of times to counts
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        map<int, int> time_bookings;
+        for (auto meeting : intervals) {
+            time_bookings[meeting.start]++;
+            time_bookings[meeting.end]--;
+        }
+
+        int max_rooms = 0;
+        int rooms = 0;
+        for (auto [_, bookings] : time_bookings) {
+            rooms += bookings; 
+            max_rooms = max(max_rooms, rooms);
+        }
+        return max_rooms;
+    }
+};
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
