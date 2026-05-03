87f8e8761803c6c02ab7e20a55880caa6473fce0
Darwin Groskleg <darwingr@users.noreply.github.com>
2026-05-03 15:29:26 -0400

    Add: evaluate-reverse-polish-notation - submission-7

---
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-7.cpp
@@ -0,0 +1,27 @@
+// Stack: ops trigger pops, vals trigger push
+//  O(N)
+//  O(N)
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> val_stack;
+
+        for (string& tok : tokens) {
+            if (isdigit(tok.back()))
+                val_stack.push(stoi(tok));
+            else {
+                int b = val_stack.top(); val_stack.pop();
+                int a = val_stack.top(); val_stack.pop();
+                if (tok == "+")
+                    val_stack.push(a + b);
+                else if (tok == "-")
+                    val_stack.push(a - b);
+                else if (tok == "*")
+                    val_stack.push(a * b);
+                else if (tok == "/")
+                    val_stack.push(a / b);
+            }
+        }
+        return val_stack.top();
+    }
+};
