3b573a62fb29d7365fa71e5b2dfa7f79a69df8d6
Darwin Groskleg <darwingr@users.noreply.github.com>
2026-05-03 18:00:13 -0400

    Add: trapping-rain-water - submission-7

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-7.cpp
@@ -0,0 +1,29 @@
+// 2-pass: 2-pointer finds biggest l & r for each i
+//  O(N)
+//  O(N)
+#include <ranges>
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        vector<pair<int,int>> biggest(height.size());
+        int biggest_l = 0;
+        int biggest_r = height.size() - 1;
+        for (int i=0; i<height.size(); i++) {
+            if (height[biggest_l] <= height[i])
+                biggest_l = i;
+            biggest[i].first = biggest_l;
+
+            int j = height.size() - 1 - i;
+            if (height[j] >= height[biggest_r])
+                biggest_r = j;
+            biggest[j].second = biggest_r;
+        }
+        
+        int total_area = 0;
+        for (auto [i, bigs] : biggest | views::enumerate) {
+            auto [l, r] = bigs;
+            total_area += max(0, min(height[l], height[r]) - height[i]);
+        }
+        return total_area;
+    }
+};
