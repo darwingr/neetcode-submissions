55e9aec1c166741bed0c32318d674236492c1bf4
Darwin Groskleg <darwingr@users.noreply.github.com>
2026-05-03 15:44:05 -0400

    Add: daily-temperatures - submission-3

---
+++ Data Structures & Algorithms/daily-temperatures/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        stack<int> index_stack;
+        vector<int> result(temperatures.size());
+        for (int i=0; i<temperatures.size(); i++) {
+            while (!index_stack.empty()
+                && temperatures[index_stack.top()] < temperatures[i]
+            ) {
+                int j = index_stack.top();
+                index_stack.pop();
+                result[j] = i - j;
+            }
+            index_stack.push(i);
+        }
+        return result;
+    }
+};
