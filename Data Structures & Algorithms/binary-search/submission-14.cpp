2b672d870e0c48dfba2082184d434568d4a8107b
Darwin Groskleg <darwingr@users.noreply.github.com>
2026-05-04 17:50:30 -0400

    Add: binary-search - submission-14

---
+++ Data Structures & Algorithms/binary-search/submission-14.cpp
@@ -0,0 +1,19 @@
+// O(N log N)
+// O(1)
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        size_t l = 0;
+        size_t r = nums.size() - 1;
+        while (l<=r && nums[l] <= target && target <= nums[r]) {
+            size_t mid = l + (r - l) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (target < nums[mid])
+                r = mid - 1;
+            else
+                l = mid + 1;
+        }
+        return -1;
+    }
+};
