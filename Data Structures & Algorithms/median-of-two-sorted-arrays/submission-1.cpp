6e8673d3070e3ae5e297ad0eaec1d56a320cc97b
Darwin Groskleg <darwingr@users.noreply.github.com>
2026-05-13 16:52:05 -0400

    Add: median-of-two-sorted-arrays - submission-1

---
+++ Data Structures & Algorithms/median-of-two-sorted-arrays/submission-1.cpp
@@ -0,0 +1,16 @@
+// Merge and calculate
+//  O(M + N)
+//  O(M + N)
+#include <ranges>
+class Solution {
+public:
+    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+        auto n = vector<int>(nums1.size() + nums2.size());
+        ranges::merge(nums1, nums2, n.begin());
+        size_t mid = n.size() / 2;
+        if (n.size() % 2 == 0)
+            return (n[mid-1] + n[mid]) / 2.0;
+        else
+            return n[mid];
+    }
+};
