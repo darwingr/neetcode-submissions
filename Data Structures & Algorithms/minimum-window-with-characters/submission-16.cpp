dfbd9662b807a840a21a68b7d6292acc32c258d2
Darwin Groskleg <darwingr@users.noreply.github.com>
2026-05-12 23:31:12 -0400

    Add: minimum-window-with-characters - submission-16

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-16.cpp
@@ -0,0 +1,38 @@
+// Freq maps, track overlap count and save shortest string
+//  have/need are good variable names
+//  O(N + M)
+//  O(M)
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        if (s.size() < t.size())
+            return "";
+        
+        unordered_map<char, int> t_freqs;
+        for (char c : t)
+            t_freqs[c]++;
+
+        unordered_map<char, int> s_freqs;
+        int overlap = 0;
+        string shortest = "";
+        int l = 0;
+        for (int r = 0; r<s.size(); r++) {
+            char sr = s[r];
+            s_freqs[sr]++;
+            if (t_freqs.contains(sr) && s_freqs[sr] <= t_freqs[sr])
+                overlap++;
+
+            while (overlap == t.size()) {
+                if (r-l+1 < shortest.size() || shortest.empty())
+                    shortest = s.substr(l, r-l+1);
+                
+                char sl = s[l];
+                if (t_freqs.contains(sl) && s_freqs[sl] <= t_freqs[sl])
+                    overlap--;
+                s_freqs[sl]--;
+                l++;
+            }
+        }
+        return shortest;
+    }
+};
