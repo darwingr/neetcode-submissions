d3c914467f3d00894e540b2482d980d30eb2417c
Darwin Groskleg <darwingr@users.noreply.github.com>
2026-05-14 22:47:43 -0400

    Add: sliding-window-maximum - submission-3

---
+++ Data Structures & Algorithms/sliding-window-maximum/submission-3.cpp
@@ -0,0 +1,34 @@
+// 
+//  O(N log N)
+//  O(N)
+#include <ranges>
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        vector<int> res;
+        res.reserve(nums.size() - k + 1);
+
+        priority_queue<int> max_heap;
+        // init the window
+        for (int n : nums | views::take(k-1))
+            max_heap.push(n);
+        priority_queue<int> removal_queue;
+        
+        for (int l = 0, r = k-1; r<nums.size(); l++, r++) {
+            // handle new r
+            max_heap.push(nums[r]);
+
+            res.push_back(max_heap.top());
+
+            // handle removing l
+            removal_queue.push(nums[l]);
+
+            // run removals
+            while (!removal_queue.empty() && removal_queue.top() == max_heap.top()) {
+                removal_queue.pop();
+                max_heap.pop();
+            }
+        }
+        return res;
+    }
+};
