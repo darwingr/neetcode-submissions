22d5fd8715d7422ac6468b2cfdad694e266aa149
Darwin Groskleg <darwingr@users.noreply.github.com>
2026-06-02 16:27:20 -0400

    Add: partition-labels - submission-0

---
+++ Data Structures & Algorithms/partition-labels/submission-0.cpp
@@ -0,0 +1,31 @@
+// Greedy: freq map & 2-pointer
+//      at most 26 letter substring
+//  O(N)
+//  O(1) - since max 26 letters is constant
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+        unordered_map<char,pair<size_t,size_t>> char_lr;
+        for (size_t i=0; i<s.size(); i++) {
+            if (char_lr.contains(s[i]))
+                char_lr[s[i]].second = i;
+            else
+                char_lr[s[i]] = {i, i};
+        }
+
+        vector<int> lengths;
+        size_t l = 0;
+        size_t next_end = 0;
+        for (size_t r=0; r<s.size(); r++) {
+            auto [char_l, char_r] = char_lr[s[r]];
+            next_end = max(next_end, char_r);
+            // found end
+            if (r == next_end) {
+                lengths.push_back(r - l + 1);
+                l = r + 1;
+            }
+        }
+
+        return lengths;
+    }
+};
