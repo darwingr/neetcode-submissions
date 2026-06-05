793c9c41b34f6331a27c3640f1631a599f61ce20
Darwin Groskleg <darwingr@users.noreply.github.com>
2026-06-05 16:32:20 -0400

    Add: subsets - submission-7

---
+++ Data Structures & Algorithms/subsets/submission-7.cpp
@@ -0,0 +1,27 @@
+// Backtracking: add current path then explore extensions,
+//               track start and path/subset
+class Solution {
+    vector<vector<int>> res;
+
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        res.clear();
+        res.reserve(1 << nums.size());
+
+        vector<int> path;
+        
+        dfs(nums, path);
+        return res;
+    }
+
+    void dfs(const vector<int>& nums, vector<int>& path, int start=0) {
+        res.push_back(path);
+        res.reserve(nums.size());
+
+        for (int i=start; i<nums.size(); i++) {
+            path.push_back(nums[i]);
+            dfs(nums, path, i + 1);
+            path.pop_back();
+        }
+    }
+};
