f4c679c1056218e8ff396faed1cefbdc416930c4
Darwin Groskleg <darwingr@users.noreply.github.com>
2026-06-19 20:30:56 -0400

    Add: islands-and-treasure - submission-0

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.cpp
@@ -0,0 +1,45 @@
+// BFS
+//  O(M x N)
+//  O(M x N)
+class Solution {
+public:
+    void islandsAndTreasure(vector<vector<int>>& grid) {
+        // Enqueue chest cells
+        queue<pair<int,int>> cells;
+        for (int i=0; i<grid.size(); i++)
+            for (int j=0; j<grid[0].size(); j++)
+                if (grid[i][j] == 0)
+                    cells.push({i, j});
+
+        //  BFS
+        while (!cells.empty()) {
+            auto [r, c] = cells.front();
+            cells.pop();
+            // Find neighboring land cells
+            vector<pair<int,int>> neibs;
+            if (r > 0 && grid[r-1][c] >= 0)
+                neibs.push_back({r-1, c});
+            if (r < grid.size()-1 && grid[r+1][c] >= 0)
+                neibs.push_back({r+1, c});
+            if (c > 0 && grid[r][c-1] >= 0)
+                neibs.push_back({r, c-1});
+            if (c < grid[0].size()-1 && grid[r][c+1] >= 0)
+                neibs.push_back({r, c+1});
+
+            // 1. re-check current dist with neighbors
+            //for (auto [nr, nc] : neibs)
+            //    grid[r][c] = 1 + min(
+            //        grid[r][c] - 1,
+            //        grid[nr][nc]
+            //    );
+
+            // 2. set dist for next cells and enqueue
+            for (auto [nr, nc] : neibs) {
+                if (grid[nr][nc] == INT_MAX)
+                    cells.push({nr, nc});
+                grid[nr][nc] = min(grid[nr][nc], grid[r][c]+1);
+            }
+        }
+
+    }
+};
