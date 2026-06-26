c2c0598085f5aa99c39b3d7be28b76af3f450a89
Darwin Groskleg <darwingr@users.noreply.github.com>
2026-06-26 19:40:09 -0400

    Add: jump-game-ii - submission-0

---
+++ Data Structures & Algorithms/jump-game-ii/submission-0.cpp
@@ -0,0 +1,20 @@
+// Track l, r, farthest, jumps
+//  O(N)
+//  O(1)
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int res = 0;
+        int l=0, r=0;
+
+        while (r < nums.size() - 1) {
+            int farthest = 0;
+            for (int i = l; i<=r; i++)
+                farthest = max(farthest, i + nums[i]);
+            l = r + 1;
+            r = farthest;
+            res++;
+        }
+        return res;
+    }
+};
