64c2d979e5bd2c18f318c74cc3fac598552218d6
Darwin Groskleg <darwingr@users.noreply.github.com>
2026-06-27 15:46:14 -0400

    Add: reverse-integer - submission-0

---
+++ Data Structures & Algorithms/reverse-integer/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int reverse(int x) {
+        string xs = to_string(abs(x));
+        long inv = 0;
+        for (int i = 0; i<xs.size(); i++) {
+            inv += (xs[i] - '0') * static_cast<long>(powl(10, i));
+        }
+        if (x < 0) inv = -inv;
+        if (inv < INT32_MIN || INT32_MAX < inv)
+            return 0;
+        return inv;
+    }
+};
