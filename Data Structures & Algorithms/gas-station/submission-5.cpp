4770644a9be5032efa4963d6850aa87ad2061f13
Darwin Groskleg <darwingr@users.noreply.github.com>
2026-07-24 19:45:02 -0400

    Add: gas-station - submission-5

---
+++ Data Structures & Algorithms/gas-station/submission-5.cpp
@@ -0,0 +1,22 @@
+// Greedy: track total difference & start, track/update tank
+//  O(N)
+//  O(1)
+class Solution {
+public:
+    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+        int start = 0;
+        int tank = 0;
+        int total = 0;
+        for (int i=0; i<gas.size(); i++) {
+            int diff = gas[i] - cost[i];
+            total += diff;
+            tank  += diff;
+            if (tank < 0) {
+                tank = 0;
+                start = i + 1;
+            }
+        }
+
+        return total < 0 ? -1 : start;
+    }
+};
